24737dc4344eaa6d02455d5ea1c9a9b7398b4620
Sophie Bonneval <maldorana@gmail.com>
2024-01-23 16:54:07 +0000

    Replace internal database by Star Wars API

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-### GraphQL Test Server
+# GraphQL Test Server
+
+This is a really simple [GraphQL](https://github.com/apollostack/graphql-tools) test server. 
+
+- The `main` branch is using an internal databse 
+- The `star-wars-api` branch is using [Apollo's Star Wars API](https://studio.apollographql.com/public/star-wars-swapi/variant/current/home)
+- The `pokemon-api` branch is using [Apollo's Pokemon API](https://studio.apollographql.com/public/poke-gql/variant/current/home)
+
+All 3 versions are running with [Apollo Server](https://github.com/apollostack/apollo-server)
+
+## Installation
+
+Clone the repository and run `npm install`
+
+```
+git clone https://github.com/SophieBonneval/graphql-test-server.git
+cd graphql-test-server
+npm install
+```
+
+## Starting the server
+
+```
+npm start
+```
+
+The server will run on port 4000. You can change this by editing `index.js`.
--- index.js
+++ index.js
@@ -1,71 +1,27 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema.js';
-import db from './_db.js';
+import axios from 'axios';
+
+const swapiUrl = 'https://swapi-graphql.netlify.app/.netlify/functions/index';
 
 const resolvers = {
   Query: {
-    games() {
-      return db.games;
-    },
-    game(_, args) {
-      return db.games.find((game) => game.id === args.id);
-    },
-    reviews() {
-      return db.reviews;
-    },
-    review(_, args) {
-      return db.reviews.find((review) => review.id === args.id);
-    },
-    authors() {
-      return db.authors;
-    },
-    author(_, args) {
-      return db.authors.find((author) => author.id === args.id);
-    },
-  },
-  Game: {
-    reviews(game) {
-      return db.reviews.filter((review) => review.game_id === game.id);
-    },
-  },
-  Author: {
-    reviews(author) {
-      return db.reviews.filter((review) => review.author_id === author.id);
-    },
-  },
-  Review: {
-    author(review) {
-      return db.authors.find((author) => author.id === review.author_id);
-    },
-    game(review) {
-      return db.games.find((game) => game.id === review.game_id);
-    },
-  },
-  Mutation: {
-    addGame(_, args) {
-      const game = {
-        ...args.game,
-        id: String(db.games.length + 1),
-      };
-      db.games.push(game);
-      return game;
-    },
-    deleteGame(_, args) {
-      const game = db.games.find((game) => game.id === args.id);
-      if (!game) {
-        throw new Error(`Couldn't find game with id ${args.id}`);
-      }
-      db.games = db.games.filter((game) => game.id !== args.id);
-      return db.games;
-    },
-    editGame(_, args) {
-      const game = db.games.find((game) => game.id === args.id);
-      if (!game) {
-        throw new Error(`Couldn't find game with id ${args.id}`);
-      }
-      Object.assign(game, args.edits);
-      return game;
+    async planets() {
+      const response = await axios.post(swapiUrl, {
+        query: `
+          query {
+            allPlanets {
+              planets {
+                id
+                name
+              }
+            }
+          }
+        `,
+      });
+
+      return response.data.data.allPlanets.planets;
     },
   },
 };
@@ -82,4 +38,4 @@ const { url } = await startStandaloneServer(server, {
   },
 });
 
-console.log(`🚀 Server ready at post 4000`);
+console.log(`🚀 Server ready at port 4000`);
