90d15bbe09731a1a6457ea6b4726ed70a675fdfd
Sophie Bonneval <maldorana@gmail.com>
2024-01-30 15:33:23 +0000

    Use .graphql schema and add queries

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from './schema.js';
+import { readFileSync } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import axios from 'axios';
+
+// Find file directory
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Function to load GraphQL schema
+function loadSchema() {
+  const schemaFilePath = join(__dirname, './schema.graphql');
+  return readFileSync(schemaFilePath, 'utf8');
+}
 
 const swapiUrl = 'https://swapi-graphql.netlify.app/.netlify/functions/index';
 
@@ -20,15 +31,58 @@ const resolvers = {
           }
         `,
       });
-
       return response.data.data.allPlanets.planets;
+    },
+    async vehicles() {
+      const response = await axios.post(swapiUrl, {
+        query: `
+          query {
+            allVehicles {
+              vehicles {
+                name
+                filmConnection {
+                  films {
+                    title
+                    releaseDate
+                  }
+                }
+              }
+            }
+          }
+        `,
+      });
+      return response.data.data.allVehicles.vehicles;
+    },
+    async starships() {
+      const response = await axios.post(swapiUrl, {
+        query: `
+          query {
+            allStarships {
+              starships {
+                id
+                name
+                crew
+                costInCredits
+                MGLT
+                filmConnection {
+                  films {
+                    title
+                    releaseDate
+                  }
+                }
+              }
+            }
+          }
+        `,
+      });
+      return response.data.data.allStarships.starships;
     },
   },
 };
 
 // Server setup
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: loadSchema(),
   resolvers,
 });
 
